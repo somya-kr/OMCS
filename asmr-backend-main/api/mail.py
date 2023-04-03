c4998556dd13e2f5f745e319a070e2e23e91fcd4
somya-kr <somya@kgpian.iitkgp.ac.in>
2023-04-03 23:54:44 +0530

    Mail body

--- asmr-backend-main/api/mail.py
+++ asmr-backend-main/api/mail.py
@@ -8,23 +8,23 @@ def asmr_mail(subject, message, recipient):
         print("Failed to send email")
 
 def new_register_mail(user, role):
-    message = "Hi " + user + ",\nYou've registered in the ASMR Online Medical Consultation Service.\n"
+    message = "Hi " + user + ",\nYou've registered in MetBud, the online medical consultation service.\n"
     if role == 2:
         message += "Since you've registered as a doctor, you will need to wait for your account to be activated by the admin before being allowed to use the service to the fullest.\n"
     message += "Hope you have a nice time using our service.\n\n"
-    message += "Thanks and regards,\nASMR Team"
+    message += "Thanks and regards,\nMetBuds Team"
     return message
 
 def doctor_accepted(user):
-    message = "Hi " + user + ",\nYou've registered in the ASMR Online Medical Consultation Service.\n"
+    message = "Hi " + user + ",\nYou've registered in MetBud, the online medical consultation service.\n"
     message += "Your account has been approved by the admin. Please continue to login and use the service.\n"
     message += "Hope you have a nice time using our service.\n\n"
-    message += "Thanks and regards,\nASMR Team"
+    message += "Thanks and regards,\nMetBuds Team"
     return message
 
 def doctor_removed(user):
-    message = "Hi " + user + ",\nYou've been removed from the ASMR Online Medical Consultation Service.\n"
+    message = "Hi " + user + ",\nYou've been removed from MetBud, the online medical consultation service.\n"
     message += "Your account has been deleted by the admin. Please register again and contact the admin to login and use the service.\n"
     message += "Hope you have a nice time using our service.\n\n"
-    message += "Thanks and regards,\nASMR Team"
+    message += "Thanks and regards,\nMetBuds Team"
     return message
